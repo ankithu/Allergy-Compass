1d0e3eda2b61a1f3f91e95a072ca5a20c9929b4b
Ankith Udupa <ankith.udupa@gmail.com>
2019-10-12 00:46:13 -0400

    code dump

---
+++ Allergy Compass/Allergy Compass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Allergy Compass
+//
+//  Created by Ankith Udupa on 10/12/19.
+//  Copyright © 2019 Ankith Udupa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
